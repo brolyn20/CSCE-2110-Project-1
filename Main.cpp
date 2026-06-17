d39a7fe092bb77cebff8140109bb46fa7a69421a
Zackary Sturtevant <zackarysturtevant@my.unt.edu>
2026-06-17 15:58:22 -0500

    Update Main.cpp
    more spacing adjustments

--- Main.cpp
+++ Main.cpp
@@ -23,9 +23,7 @@
 #include "QueueSystem.h"
 // #include "CampusMap.h"   // Emilio do this
 // #include "StudentList.h" // Brolyn do this
-
 void displayMenu();
-
 int main() {
     QueueSystem advisingOffice;
     string mapFilename;
@@ -58,29 +56,34 @@ int main() {
         switch (choice) {
             case 1://Emilio do this
                 cout << "\n[Feature] Displaying Campus Map...\n";
-                // campusMap.display();
-                
+                // campusMap.display();               
                 break;
+            
             case 2://Emilio do this
                 cout << "\n[Feature] Exploring Location Coordinates...\n";
                 // Explore logic
                 break;
+            
             case 3://Brolyn do this
                 cout << "\n[Feature] Adding a New Student...\n";
                 // studentList.addStudent();
                 break;
+            
             case 4://Brolyn do this
                 cout << "\n[Feature] Removing a Student...\n";
                 // studentList.removeStudent();
                 break;
+            
             case 5://Brolyn do this
                 cout << "\n[Feature] Searching for a Student...\n";
                 // studentList.searchStudent();
                 break;
+            
             case 6://Brolyn do this
                 cout << "\n[Feature] Sorting Student Records...\n";
                 // studentList.sortByID();
                 break;
+            
             case 7: {
                 int id;
                 string issue;
@@ -89,13 +92,13 @@ int main() {
                 cin.ignore (numeric_limits<streamsize>::max(),  '\n'); // Clear newline buffer
                 cout << "Enter brief issue description: ";
                 getline(cin, issue);
-                
                 advisingOffice.addRequest(id, issue);
                 break;
             }
             case 8:
                 advisingOffice.processNextRequest();
                 break;
+            
             case 9:
                 cout << "\n================ CAMPUS STATISTICS ================\n";
                 cout << "Pending Advising Requests: " << advisingOffice.getPendingCount() << "\n";
@@ -103,18 +106,18 @@ int main() {
                 cout << "Average GPA:    [Link StudentList data here]\n";//Brolyn do this
                 cout << "===================================================\n";
                 break;
+            
             case 10:
                 cout << "\nExiting the system. Thank you!\n";
                 break;
+            
             default:
                 cout << "Invalid choice! Please select an option between 1 and 10.\n";
                 break;
         }
     }
-
     return 0;
 }
-
 void displayMenu() {
     cout << "\n--- MAIN SYSTEM MENU ---\n";
     cout << "1. Display Campus Map\n";
