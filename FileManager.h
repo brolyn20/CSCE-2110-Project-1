6035c7cba7d9738a0f0acd44cffca1dae367453d
najeebr1 <rafeeq.najeeb10@gmail.com>
2026-06-20 16:34:26 -0500

    Add FileManager loadStudents implementation and students.txt

--- FileManager.h
+++ FileManager.h
@@ -1,1 +1,12 @@
-temp
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
+#include <string>
+#include "StudentList.h"
+ //include campus map when its done
+
+class FileManager {
+public:
+    static bool loadStudents(const std::string& filename, StudentList& studentList);
+};
+
+#endif //
